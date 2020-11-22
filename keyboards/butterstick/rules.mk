4bc606a4dcc65d784acd87fd260044f510eec822
Spencer Williams <s@spencerenglish.com>
2020-11-21 21:10:50 -0800

    basic layer for butterstick

--- keyboards/butterstick/rules.mk
+++ keyboards/butterstick/rules.mk
@@ -16,8 +16,7 @@ EXTRAKEY_ENABLE 	= yes       # Audio control and System control
 CONSOLE_ENABLE 		= yes       # Console for debug
 COMMAND_ENABLE 		= no		# Commands for debug and configuration
 NKRO_ENABLE 		= yes       # USB Nkey Rollover
-STENO_ENABLE		= yes 		# Needed for chording
+STENO_ENABLE		= no 		# Needed for chording
 
 OPT_DEFS += -DONLYQWERTY -DDEBUG_MATRIX
-SRC += sten.c
 EXTRAFLAGS += -flto
