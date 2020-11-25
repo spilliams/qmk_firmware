2f5dafc528745cf1c93dccd239a1307a12041518
Spencer Williams <s@spencerenglish.com>
2020-11-25 10:23:45 -0800

    symbol layer should have manual escape, less overlap with symbol dictionary

--- keyboards/georgi/keymaps/spilliams/keymap.c
+++ keyboards/georgi/keymaps/spilliams/keymap.c
@@ -36,7 +36,7 @@
 //
 // http://docs.gboards.ca
 uint32_t processQwerty(bool lookup) {
-    SEND_STRING("v4.0.0 spilliams ");
+    SEND_STRING("v4.0.1 spilliams ");
     SEND_STRING(__DATE__);
     return 0;
 }
@@ -59,19 +59,19 @@ uint32_t processQwerty(bool lookup) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Main layer, everything goes through here
     [L0] = LAYOUT_georgi(
-    KC_ESC,  STN_S1,  STN_TL,   STN_PL,   STN_HL,  STN_ST1,     STN_ST3,  STN_FR,  STN_PR,  STN_LR,  STN_TR,   STN_DR,
-    OSL(L1), STN_S2,  STN_KL,   STN_WL,   STN_RL,  STN_ST2,     STN_ST4,  STN_RR,  STN_BR,  STN_GR,  STN_SR,   STN_ZR,
-                                STN_N1,   STN_A,   STN_O,       STN_E,    STN_U,   STN_N7
+    KC_ESC,  STN_S1,  STN_TL,   STN_PL,   STN_HL,   STN_ST1,     STN_ST3,  STN_FR,  STN_PR,   STN_LR,  STN_TR,   STN_DR,
+    OSL(L1), STN_S2,  STN_KL,   STN_WL,   STN_RL,   STN_ST2,     STN_ST4,  STN_RR,  STN_BR,   STN_GR,  STN_SR,   STN_ZR,
+                                STN_N1,   STN_A,    STN_O,       STN_E,    STN_U,   STN_N7
     ),
     [L1] = LAYOUT_georgi(
-    KC_TRNS, KC_W,    KC_GESC,  KC_E,     KC_R,    KC_T,        KC_1,     KC_2,    KC_3,    KC_4,    KC_P,     KC_COMMA,
-    KC_TRNS, KC_LSFT, KC_TAB,   KC_S,     KC_F,    KC_G,        KC_0,     KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, KC_SLASH,
-                                KC_LCTRL, KC_LALT, KC_LGUI,     KC_SPACE, KC_RSFT, OSL(L2)
+    TO(L0),  KC_1,    KC_2,     KC_3,     KC_4,     KC_5,        KC_NO,    KC_6,    KC_7,     KC_8,    KC_9,     KC_0,
+    KC_TRNS, KC_TAB,  KC_GESC,  KC_NO,    KC_NO,    KC_NO,       KC_MINUS, KC_PLUS, KC_COMMA, KC_DOT,  KC_SLASH, KC_BSLASH,
+                                KC_LSFT,  KC_LCTRL, KC_LALT,     KC_LGUI,  KC_NO,   OSL(L2)
     ),
     [L2] = LAYOUT_georgi(
-    KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,       KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,
-    KC_NO,   KC_MPRV, KC_MPLY,  KC_MNXT,  KC_VOLD, KC_VOLU,     KC_MUTE,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_SYSTEM_SLEEP,
-                                KC_TRNS,  KC_TRNS, KC_TRNS,     KC_NO,    KC_TRNS, KC_TRNS
+    KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,    KC_F6,       KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,
+    KC_NO,   KC_MPRV, KC_MPLY,  KC_MNXT,  KC_VOLD,  KC_VOLU,     KC_MUTE,  KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_SYSTEM_SLEEP,
+                                KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS
     )
 };
 // Don't fuck with this, thanks.
