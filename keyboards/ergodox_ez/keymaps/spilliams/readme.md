b63486a71139596d84b054c6cd0db698fecadc3a
Spencer Williams <s@spencerenglish.com>
2021-03-22 19:16:20 -0700

    start with a basic layout (and complex rules)

---
+++ keyboards/ergodox_ez/keymaps/spilliams/readme.md
@@ -0,0 +1,5 @@
+# Ergodox EZ keymap
+
+This is spilliams' keymap for Ergodox EZ Glow.
+
+It combines ergodox-ez/steno, ergodox/default_glow, and georgi/spilliams.
---
+++ keyboards/ergodox_ez/keymaps/spilliams/rules.mk
@@ -0,0 +1,18 @@
+# from ergodox/steno
+STENO_ENABLE = yes          # Additional protocols for Stenography(+1700), requires VIRTSER
+
+# from georgi/spilliams
+#Firmware reduction options
+MOUSEKEY_ENABLE			 = no   # 1500 bytes
+NO_TAPPING				 = no   # this also enables OSL, for some reason
+NO_PRINT				 = yes
+
+#Debug options
+CONSOLE_ENABLE			 = no   # Plover needs this to be no? to use the serial?
+DEBUG_MATRIX_SCAN_RATE   = no
+DEBUG_MATRIX			 = no
+ONLY_QWERTY				 = no
+
+# from ergodox_ez/default_glow
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes # enable later
